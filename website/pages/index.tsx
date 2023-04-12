3809a466de4b6cbca01f0fc3f094db4e3b1fc092
nkrulikovsky <nkrulikovskiy@gmail.com>
2023-04-12 03:28:32 +0300

    Init

---
+++ website/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center text-center">
+      <p>Coming Soon</p>
+      <p className="mt-2">⌐◨-◨</p>
+    </main>
+  );
+}
