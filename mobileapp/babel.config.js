7c3c2e7e438dd492e56128f4e6ab526b287430a0
nkrulikovsky <nkrulikovskiy@gmail.com>
2023-04-27 22:36:51 +0300

    Remove packages that prevented fast refresh

--- mobileapp/babel.config.js
+++ mobileapp/babel.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    "nativewind/babel",
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-proposal-private-methods"
+    "nativewind/babel"
   ],
 };
