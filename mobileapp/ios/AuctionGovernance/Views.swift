eb8cb99f279ce203f1238a407da358a543858235
nkrulikovsky <nkrulikovskiy@gmail.com>
2023-05-09 12:24:14 +0300

    Better classes structure

--- mobileapp/ios/AuctionGovernance/Views.swift
+++ mobileapp/ios/AuctionGovernance/Views.swift
@@ -32,7 +32,49 @@ struct ProposalView: View {
   let proposal: ProposalData
   let displayType: ProposalDisplayType
   
+  let timeLeft: Double
+  let endsIn: String
+  
+  let timeLeftColor: Color
+  let timeLeftBorder: Color
+  
+  let stateColor: Color
+  let stateBorder: Color
+  
   @Environment(\.colorScheme) var colorScheme
+  
+  init(proposal: ProposalData, displayType: ProposalDisplayType) {
+    self.proposal = proposal
+    self.displayType = displayType
+    
+    timeLeft = max(0, Double(proposal.endTime) - Date().timeIntervalSince1970)
+    
+    let formatter = RelativeDateTimeFormatter()
+    formatter.unitsStyle = .short
+    formatter.dateTimeStyle = .numeric
+    formatter.locale = Locale(identifier: "en-US")
+    
+    endsIn = formatter.localizedString(
+      for: Date(timeIntervalSince1970: proposal.endTime),
+      relativeTo: Date.now
+    )
+    
+    if (timeLeft <= 43200) {
+      timeLeftColor = Color(red: 1, green: 0.226, blue: 0.226)
+      timeLeftBorder = timeLeftColor.opacity(0.3)
+    } else {
+      timeLeftColor = Color(red: 0.55, green: 0.55, blue: 0.55)
+      timeLeftBorder = Color(red: 0.80, green: 0.80, blue: 0.80)
+    }
+    
+    if (proposal.state == "ACTIVE") {
+      stateColor = Color(red: 1, green: 0.226, blue: 0.226)
+      stateBorder = timeLeftColor.opacity(0.3)
+    } else {
+      stateColor = Color(red: 0.55, green: 0.55, blue: 0.55)
+      stateBorder = Color(red: 0.80, green: 0.80, blue: 0.80)
+    }
+  }
   
   var body: some View {
     switch displayType {
@@ -46,38 +88,18 @@ struct ProposalView: View {
   }
   
   var compact: some View {
-    var color = Color(red: 0.55, green: 0.55, blue: 0.55)
-    var borderColor = Color(red: 0.80, green: 0.80, blue: 0.80)
-    
-    let timeLeft = max(0, Double(proposal.endTime) - Date().timeIntervalSince1970)
-    
-    if (timeLeft <= 43200) {
-      color = Color(red: 1, green: 0.226, blue: 0.226)
-      borderColor = color.opacity(0.3)
-    }
-    
-    let formatter = RelativeDateTimeFormatter()
-    formatter.unitsStyle = .short
-    formatter.dateTimeStyle = .numeric
-    formatter.locale = Locale(identifier: "en-US")
-
-    let endsIn = formatter.localizedString(
-      for: Date(timeIntervalSince1970: proposal.endTime),
-      relativeTo: Date.now
-    )
-    
     return HStack(alignment: .center, spacing: 4) {
       ZStack {
         Text("Ends \(endsIn)")
           .font(.system(size: 10, weight: .bold))
-          .foregroundColor(color)
+          .foregroundColor(timeLeftColor)
           .padding(.horizontal, 2)
       }
       .cornerRadius(2)
       .overlay(
         RoundedRectangle(cornerRadius: 2)
           .stroke(
-            borderColor,
+            timeLeftBorder,
             lineWidth: 1
           )
       )
