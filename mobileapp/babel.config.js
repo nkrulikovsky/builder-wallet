b2119f8305c1d66e2de12a709a6c4b8fb114b26e
nkrulikovsky <nkrulikovskiy@gmail.com>
2023-05-12 05:13:10 +0300

    Add ethers required packages

--- mobileapp/babel.config.js
+++ mobileapp/babel.config.js
@@ -2,6 +2,8 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     "nativewind/babel",
-    "module:react-native-dotenv"
+    "module:react-native-dotenv",
+    ["@babel/plugin-proposal-private-methods", { loose: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
   ],
 };
--- mobileapp/src/components/WalletSection/index.tsx
+++ mobileapp/src/components/WalletSection/index.tsx
@@ -1,17 +1,35 @@
 import { useWeb3Modal } from '@web3modal/react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { Image, Pressable, Text, View } from 'react-native'
 import ConnectWalletButton from '../ConnectWalletButton'
+import { ethers, TypedDataDomain, TypedDataField } from 'ethers'
 
 const WalletSection = ({}) => {
   const { isOpen, open, close, provider, isConnected } = useWeb3Modal()
 
+  const [address, setAddress] = useState<string | null>(null)
+
+  // const web3Provider = useMemo(
+  //   () => (provider ? new ethers.BrowserProvider(provider) : undefined),
+  //   [provider]
+  // )
+
+  // useEffect(() => {
+  //   const getAddress = async () => {
+  //     if (web3Provider) {
+  //       const [address] = await web3Provider.listAccounts()
+  //       setAddress(address.address)
+  //     }
+  //   }
+  //   getAddress()
+  // }, [web3Provider])
+
   return (
     <View className="flex flex-col">
-      <View className="w-full bg-grey-one/70 rounded-lg p-4 mb-3">
-        <Text className="text-xl font-bold">Add your wallet</Text>
-        <Text className="">Connect to automatically follow your Daos</Text>
-        {!isConnected && (
+      {!isConnected && (
+        <View className="w-full bg-grey-one/70 rounded-lg p-4 mb-3">
+          <Text className="text-xl font-bold">Add your wallet</Text>
+          <Text className="">Connect to automatically follow your Daos</Text>
           <View className="mt-4 -mx-4">
             {/* TODO: dynamically set image size */}
             <Image
@@ -19,8 +37,14 @@ const WalletSection = ({}) => {
               source={require('../../assets/img/dao-examples-horizontal.png')}
             />
           </View>
-        )}
-      </View>
+        </View>
+      )}
+      {address && (
+        <View className="mt-4 -mx-4">
+          <Text className="text-xl font-bold">Address</Text>
+          <Text className="text-sm">{address}</Text>
+        </View>
+      )}
       <ConnectWalletButton />
     </View>
   )
