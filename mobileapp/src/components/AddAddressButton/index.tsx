b9e48a8a4ef06f4f08f0082b0ee7ce20a8b07e05
nkrulikovsky <nkrulikovskiy@gmail.com>
2023-05-24 17:47:49 +0300

    Different clipboard logic

--- mobileapp/src/components/AddAddressButton/index.tsx
+++ mobileapp/src/components/AddAddressButton/index.tsx
@@ -1,16 +1,12 @@
 import clsx from 'clsx'
 import React, { useState } from 'react'
 import { Alert, Modal, Pressable, Text, TextInput, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
 import Svg, { Path } from 'react-native-svg'
-import { useClipboard } from '@react-native-clipboard/clipboard'
+import Clipboard from '@react-native-clipboard/clipboard'
 import { isAddress } from 'ethers'
 import { useAddressesStore } from '../../store/addresses'
 
 const AddAddressButton = ({}) => {
-  // TODO: gives paste alert
-  const [clipboardData] = useClipboard()
-
   const addAddress = useAddressesStore(state => state.addManualAddress)
 
   const [modalVisible, setModalVisible] = useState(false)
@@ -20,8 +16,14 @@ const AddAddressButton = ({}) => {
     setAddressText(String(text).trim())
   }
 
+  const pasteClipboardData = async () => {
+    const text = await Clipboard.getString()
+    if (text) {
+      setAddressText(text)
+    }
+  }
+
   const validAddress = isAddress(addressText)
-  const hasClipboardData = clipboardData.length > 0
 
   return (
     <View>
@@ -48,10 +50,7 @@ const AddAddressButton = ({}) => {
                   Enter Ethereum Address
                 </Text>
                 <View className="flex flex-row h-12 bg-grey-one rounded-lg justify-between items-center">
-                  <View
-                    className={clsx(
-                      hasClipboardData ? 'w-[88%] pl-4' : 'px-4'
-                    )}>
+                  <View className="w-[88%] pl-4">
                     <TextInput
                       autoFocus={true}
                       onChangeText={handleChangeText}
@@ -59,23 +58,21 @@ const AddAddressButton = ({}) => {
                       placeholder="Address"
                     />
                   </View>
-                  {hasClipboardData && (
-                    <Pressable
-                      className="h-12 w-12 items-center justify-center flex-none"
-                      onPress={() => setAddressText(clipboardData)}>
-                      <Svg
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        className="w-6 h-6 stroke-grey-four">
-                        <Path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
-                        />
-                      </Svg>
-                    </Pressable>
-                  )}
+                  <Pressable
+                    className="h-12 w-12 items-center justify-center flex-none"
+                    onPress={pasteClipboardData}>
+                    <Svg
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      className="w-6 h-6 stroke-grey-four">
+                      <Path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
+                      />
+                    </Svg>
+                  </Pressable>
                 </View>
                 <Pressable
                   onPress={() => {
