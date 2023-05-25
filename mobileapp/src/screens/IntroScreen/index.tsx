f2f77c535b061ad5cf94fd672663f7048efe1b1b
nkrulikovsky <nkrulikovskiy@gmail.com>
2023-05-25 14:02:22 +0300

    Add daos views

--- mobileapp/src/screens/IntroScreen/index.tsx
+++ mobileapp/src/screens/IntroScreen/index.tsx
@@ -1,13 +1,9 @@
-import { Pressable, Text, TouchableOpacity, View } from 'react-native'
+import { Image, Pressable, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { useDaosStore } from '../../store/daos'
 import { RootStackScreenProps } from '../../navigation/types'
-import clsx from 'clsx'
-import DaoCardImage from '../../components/DaoCardImage'
-import Countdown from '../../components/Countdown'
-import Svg, { Path } from 'react-native-svg'
 import { IntroStage, useIntroStore } from '../../store/intro'
 import { useEffect } from 'react'
+import LinearGradient from 'react-native-linear-gradient'
 
 const IntroScreen = ({ route, navigation }: RootStackScreenProps<'Intro'>) => {
   const introStage = useIntroStore(state => state.stage)
@@ -56,16 +52,45 @@ const IntroScreen = ({ route, navigation }: RootStackScreenProps<'Intro'>) => {
       break
     case IntroStage.ADD_DAOS:
       view = (
-        <View className="mx-4 h-full">
-          <Text>Intro. Stage ADD_DAOS</Text>
-          <Pressable
-            onPress={() => {
-              setIntroStage(IntroStage.DONE)
-            }}>
-            <View className="h-12 w-full bg-black rounded-lg items-center justify-center text-center">
-              <Text className="text-white">Next</Text>
-            </View>
-          </Pressable>
+        <View className="h-full">
+          <View className="mt-1">
+            <Image
+              className="w-full h-[90%]"
+              source={require('../../assets/img/dao-images-grid.png')}
+            />
+          </View>
+          <View className="absolute bottom-0 left-0 right-0">
+            <LinearGradient
+              locations={[0, 0.42]}
+              colors={['rgba(255, 255, 255, 0)', 'rgba(255, 255, 255, 1)']}
+              className="flex flex-col gap-3 px-4 pb-3 pt-32">
+              <Text className="text-black font-extrabold text-4xl -mb-1">
+                Add DAOs
+              </Text>
+              <Text className="text-black text-lg">
+                Add your wallet to automatically load all your DAOs or search
+                for your DAOs manually
+              </Text>
+              <Pressable
+                onPress={() => {
+                  // TODO
+                  setIntroStage(IntroStage.DONE)
+                }}>
+                <View className="h-12 w-full bg-black rounded-lg items-center justify-center text-center">
+                  <Text className="text-white">Add Wallet</Text>
+                </View>
+              </Pressable>
+              <Pressable
+                onPress={() => {
+                  // TODO
+                  setIntroStage(IntroStage.DONE)
+                }}>
+                <View className="h-12 w-full bg-grey-one rounded-lg items-center justify-center text-center">
+                  <Text className="text-black">Search</Text>
+                </View>
+              </Pressable>
+            </LinearGradient>
+          </View>
         </View>
       )
       break
