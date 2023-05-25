b8369bd256ce50b98da33e91fbf19be221eeb00c
nkrulikovsky <nkrulikovskiy@gmail.com>
2023-05-25 20:14:50 +0300

    Add todo

--- mobileapp/src/constants/viemWagmi.ts
+++ mobileapp/src/constants/viemWagmi.ts
@@ -1,6 +1,7 @@
 import { WagmiConfig, createConfig, configureChains, mainnet } from 'wagmi'
 import { publicProvider } from 'wagmi/providers/public'
 
+//TODO: Add own providers
 export const { chains, publicClient, webSocketPublicClient } = configureChains(
   [mainnet],
   [publicProvider()]
