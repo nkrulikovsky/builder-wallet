cf4782fa17807ec6197e56bf49463934dc478a00
nkrulikovsky <nkrulikovskiy@gmail.com>
2023-06-27 04:30:05 +0300

    Posthog analytics

--- mobileapp/src/constants/mixpanel.ts
+++
@@ -1,6 +0,0 @@
-import { Mixpanel } from 'mixpanel-react-native'
-
-// @ts-expect-error - `@env` is a virtualised module via Babel config.
-import { MIXPANEL_TOKEN } from '@env'
-
-export const mixpanel = new Mixpanel(MIXPANEL_TOKEN, true)
