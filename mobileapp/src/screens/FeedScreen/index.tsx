f7ab6e18e12bcf09ae010aaabb662ea98b677e6a
nkrulikovsky <nkrulikovskiy@gmail.com>
2023-06-28 16:13:50 +0300

    Sort props order

--- mobileapp/src/screens/FeedScreen/index.tsx
+++ mobileapp/src/screens/FeedScreen/index.tsx
@@ -74,6 +74,31 @@ const FeedScreen = ({ route, navigation }: HomeTabScreenProps<'Feed'>) => {
       p.status === 'ACTIVE' || p.status === 'PENDING' || p.status === 'QUEUED'
   )
 
+  props.sort((a, b) => {
+    const statusOrder = ['ACTIVE', 'PENDING', 'QUEUED']
+
+    // Compare the status order
+    const statusComparison =
+      statusOrder.indexOf(a.status) - statusOrder.indexOf(b.status)
+
+    if (statusComparison === 0) {
+      // Same status, perform additional sorting
+      if (a.status === 'ACTIVE') {
+        return a.voteEnd - b.voteEnd
+      } else if (a.status === 'PENDING') {
+        return a.voteStart - b.voteStart
+      } else if (
+        a.status === 'QUEUED' &&
+        a.executableFrom &&
+        b.executableFrom
+      ) {
+        return a.executableFrom - b.executableFrom
+      }
+    }
+
+    return statusComparison
+  })
+
   return (
     <ScrollView
       className="flex flex-col h-full bg-white"
