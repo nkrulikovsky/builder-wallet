433c37c74f9f2a165e1fd848c9d4e301e34d93bc
nkrulikovsky <nkrulikovskiy@gmail.com>
2023-06-30 02:28:34 +0300

    Promo page for builder app

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,14 +1,77 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-gradient-to-b from-white to-sky/30 text-black">
-      <div className="flex flex-col sm:flex-row mt-8 sm:mt-20 justify-center">
-        <div></div>
-        <div>
-          <p className="text-2xl font-black">Builder DAOs in the pocket</p>
-          <p>Track all auctions</p>
-          <p>See all pending proposals</p>
+    <main className="flex min-h-screen flex-col bg-gradient-to-b from-white to-sky/50 text-black">
+      <div className="flex flex-col-reverse md:flex-row mt-12 md:mt-20 mb-20 justify-center items-center gap-8 md:gap-20 lg:gap-32 px-8 text-center md:text-start">
+        <div className="">
+          <video
+            autoPlay
+            loop
+            muted
+            playsInline
+            className="w-72 rounded-[2.5rem] border-[7px] border-black ring-[3px] ring-[#7e7384] rotate-0 md:-rotate-1"
+            src="/video/showcase.mp4"
+          />
+        </div>
+        <div className="pb-4 md:pb-12">
+          <h1 className="text-2xl sm:text-3xl md:text-4xl font-black">
+            Your Builder DAOs
+            <br />
+            in the pocket
+          </h1>
+          <div className="mt-8">
+            <p>
+              <span className="text-pink">•</span> All your Daos in one place
+            </p>
+            <p>
+              <span className="text-purple">•</span> See pending proposals
+            </p>
+            <p>
+              <span className="text-green">•</span> Track auctions
+            </p>
+            <p>
+              <span className="text-orange">•</span> Widgets for everything
+            </p>
+            <p>
+              <span className="text-red">•</span> Vote
+              <span className="text-black">&</span>Bid via in-app browser
+            </p>
+          </div>
+          <div className="mt-8 max-w-max hidden md:flex flex-row bg-white rounded-2xl p-2 gap-1">
+            <img
+              src="/svg/app-store-qr.svg"
+              alt="App Store Install QR Code"
+              className="h-32 w-32"
+            />
+            <div className="flex flex-col justify-end items-start">
+              <p className="pl-2 pt-2 my-auto text-sm text-grey-four leading-4">
+                Scan to download
+                <br />
+                or click bellow 👇
+              </p>
+              <a
+                href="https://testflight.apple.com/join/Ja91wWG3"
+                target="_blank"
+              >
+                <img
+                  src="/svg/app-store-download-badge.svg"
+                  alt="App Store Download Badge"
+                  className="w-32 m-1.5"
+                />
+              </a>
+            </div>
+          </div>
+          <div className="mt-8 flex md:hidden w-full justify-center">
+            <a
+              href="https://testflight.apple.com/join/Ja91wWG3"
+              target="_blank"
+            >
+              <img
+                src="/svg/app-store-download-badge.svg"
+                alt="App Store Download Badge"
+                className="w-32 m-1.5"
+              />
+            </a>
+          </div>
         </div>
       </div>
     </main>
