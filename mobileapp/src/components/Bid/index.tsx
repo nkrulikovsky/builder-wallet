dac68c943645b0a2a6449c8afee4dc26db14f0db
nkrulikovsky <nkrulikovskiy@gmail.com>
2023-07-26 03:56:51 +0300

    Text if there is no bids

--- mobileapp/src/components/Bid/index.tsx
+++ mobileapp/src/components/Bid/index.tsx
@@ -17,6 +17,7 @@ const Bidder = ({ address, bid, className }: BidderProps) => {
     name: ens
   })
 
+  const noBid = Number(bid) === 0
   const displayName = `${ens ?? shortAddress(address)}`
   const displayBid = `${bid} Ξ`
   const displayAvatar = avatar
@@ -24,21 +25,27 @@ const Bidder = ({ address, bid, className }: BidderProps) => {
   return (
     <View
       className={clsx(
-        'flex flex-row border border-grey-one pl-3 pr-4 h-12 w-full rounded-lg items-center justify-between',
+        'border border-grey-one px-4 h-12 w-full rounded-lg',
         className
       )}>
-      <View className="flex flex-row text-center items-center">
-        {displayAvatar && (
-          <Image
-            className="h-6 w-6 mr-1 rounded-full bg-grey-one/60"
-            source={{
-              uri: displayAvatar
-            }}
-          />
-        )}
-        <Text className="text-black ml-1">{displayName}</Text>
-      </View>
-      <Text className="text-black">{displayBid}</Text>
+      {noBid ? (
+        <Text className="my-auto">No bids!</Text>
+      ) : (
+        <View className="-ml-1 h-full flex flex-row items-center justify-between">
+          <View className="flex flex-row text-center items-center">
+            {displayAvatar && (
+              <Image
+                className="h-6 w-6 mr-1 rounded-full bg-grey-one/60"
+                source={{
+                  uri: displayAvatar
+                }}
+              />
+            )}
+            <Text className="text-black ml-1">{displayName}</Text>
+          </View>
+          <Text className="text-black">{displayBid}</Text>
+        </View>
+      )}
     </View>
   )
 }
