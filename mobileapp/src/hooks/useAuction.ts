d0fdfab0ca9a34210001d8ce7426ecb8b55374b9
nkrulikovsky <nkrulikovskiy@gmail.com>
2023-11-26 01:26:08 +0200

    Fix time problem

--- mobileapp/src/hooks/useAuction.ts
+++ mobileapp/src/hooks/useAuction.ts
@@ -1,6 +1,7 @@
 import { ApolloError, useQuery } from '@apollo/client'
 import { DAO_QUERY } from '../constants/queries'
 import { BuilderDAOsAuctionResponse, CurrentAuction } from '../utils/types'
+import { ensureMilliseconds } from '../utils/time'
 
 export default function useAuction(dao: string) {
   const {
@@ -22,7 +23,7 @@ export default function useAuction(dao: string) {
     data?.auctions && data?.auctions[0]
 
   if (auction) {
-    auction.endTime = auction.endTime * 1000
+    auction.endTime = ensureMilliseconds(auction.endTime)
   }
 
   return { auction, loading, error }
